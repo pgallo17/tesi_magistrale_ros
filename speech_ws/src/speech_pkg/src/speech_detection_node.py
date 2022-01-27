fa7acf74792639f4f730fddb323ce575f1dc0ffe
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2022-01-27 17:53:53 +0100

    Update speech_detection_node.py

--- speech_ws/src/speech_pkg/src/speech_detection_node.py
+++ speech_ws/src/speech_pkg/src/speech_detection_node.py
@@ -150,7 +150,7 @@ if __name__ == '__main__':
     print("Waiting manager service")
     rospy.wait_for_service('manager_service')
     print("manager service found")
-    manger_service = rospy.ServiceProxy('manager', Manager)
+    manger_service = rospy.ServiceProxy('manager_service', Manager)
     speech_detection = SpeechDetectionNode()
     speech_detection.start()
     
