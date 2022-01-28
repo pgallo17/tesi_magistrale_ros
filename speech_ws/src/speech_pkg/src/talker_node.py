4416cd62eee1521f6f67656b412e521e49677d68
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2022-01-28 11:14:44 +0100

    Update talker_node.py

--- speech_ws/src/speech_pkg/src/talker_node.py
+++ speech_ws/src/speech_pkg/src/talker_node.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     # Connect to the robot
     print("Connecting to robot...")
     session = qi.Session()
-    session.connect('tcp://%s:%s' % (pepper.ip, pepper.port))  # Robot IP
+    session.connect('tcp://%s:%s' % (pepper.ip, str(pepper.port)))  # Robot IP
     print("Robot connected")
 
     # TextToSpeech service
