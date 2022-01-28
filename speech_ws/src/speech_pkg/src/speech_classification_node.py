46ad84edb6be7f723a55bcf7a48b7295059424de
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2022-01-28 09:36:08 +0100

    Update speech_classification_node.py

--- speech_ws/src/speech_pkg/src/speech_classification_node.py
+++ speech_ws/src/speech_pkg/src/speech_classification_node.py
@@ -79,7 +79,8 @@ class Classifier:
         probs = self.model.predict(logits)
         probs = probs.cpu().detach().numpy()
         cmd = np.argmax(probs, axis=1)
-        return cmd, probs
+        response = ClassificationResponse(cmd, probs)
+        return response
 
     def parse_req(self, req):
         signal = self.convert(req.data.data)
