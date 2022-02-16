cd2bc4c6fae2b05bf69d908e50bbd218c8f14467
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2022-02-16 17:34:12 +0100

    Update vad_analysis.py

--- speech_ws/src/speech_pkg/src/vad_analysis.py
+++ speech_ws/src/speech_pkg/src/vad_analysis.py
@@ -5,14 +5,13 @@ import numpy as np
 
 if __name__ == "__main__":
     th = 0.5
-    duration = 2
     chunk = [480, 960, 1600]
     silero = MySileroVad(threshold=0.5, sampling_rate=16000)
 
     for ch in chunk:
         latency= []
+        audio_data = np.random.rand(ch)
         for i in range(200):
-            audio_data = np.random.rand(ch)
             data = audio_data.tobytes()
             start_time = time.time()
             silero.is_speech(data)
