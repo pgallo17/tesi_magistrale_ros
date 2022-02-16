52d0f444702102061c55e269cf66d48cad8ba697
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2022-02-16 16:25:32 +0100

    Update vad_analysis.py

--- speech_ws/src/speech_pkg/src/vad_analysis.py
+++ speech_ws/src/speech_pkg/src/vad_analysis.py
@@ -2,21 +2,24 @@ from utils import MySileroVad
 import librosa
 import numpy as np
 import time
+import random
 
 if __name__ == "__main__":
     th = 0.5
     duration = 2
     chunk = [480, 960, 1600]
     silero = MySileroVad(threshold=0.5, sampling_rate=16000)
-    audio_data = librosa.tone(frequency=512, sr=16000, duration=duration)
+
     latency = []
     for ch in chunk:
-        split = len(audio_data) // ch
-        audio_data_split = np.array_split(audio_data, split)
         for i in range(200):
+            audio_data = librosa.tone(frequency=random.randint(100, 512), sr=16000, duration=duration)
+            split = len(audio_data) // ch
+            audio_data_split = np.array_split(audio_data, split)
             for e in audio_data_split:
+                data = e.tobytes()
                 start_time = time.time()
-                silero.is_speech(e.tobytes())
+                silero.is_speech(data)
                 end_time = time.time()
                 latency.append(end_time-start_time)
         mean_value = np.array(latency).mean()
