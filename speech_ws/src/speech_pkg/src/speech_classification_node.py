2d82282a9ffb3997a6c678a3a3252d06168e2f24
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2022-02-17 12:36:21 +0100

    Update speech_classification_node.py

--- speech_ws/src/speech_pkg/src/speech_classification_node.py
+++ speech_ws/src/speech_pkg/src/speech_classification_node.py
@@ -87,7 +87,11 @@ class Classifier:
         probs = self.model.predict(logits)
         probs = probs.cpu().detach().numpy()
         REJECT_LABEL = probs.shape[1] - 1
-        cmd = np.argmax(probs, axis=1)
+        if probs[0, REJECT_LABEL] >= 0.23:
+            cmd = np.array([REJECT_LABEL])
+            print(cmd.shape)
+        else:
+            cmd = np.argmax(probs, axis=1)
         return cmd, probs
 
     def parse_req(self, req):
