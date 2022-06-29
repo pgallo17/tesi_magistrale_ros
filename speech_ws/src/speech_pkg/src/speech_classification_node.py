eefb6efa116a9944aad9fe03acca5dfefc663fb7
pgallo17 <p.gallo17@studenti.unisa.it>
2022-06-29 08:33:02 +0200

    29/06/2022_08:33

--- speech_ws/src/speech_pkg/src/speech_classification_node.py
+++ speech_ws/src/speech_pkg/src/speech_classification_node.py
@@ -52,7 +52,7 @@ class Classifier:
 
     def predict_cmd(self, signal: np.ndarray):
         x=np.reshape(signal,(1,signal.shape[0],1))
-        _,y=self.model.predict(x)
+        emb,y=self.model.predict(x)
         #print(y[0])
         l=[]
         for ele in y[0]:
