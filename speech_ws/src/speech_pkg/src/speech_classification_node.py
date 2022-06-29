a651c2113823db508ab9904e0841b8a4d2a8279c
pgallo17 <p.gallo17@studenti.unisa.it>
2022-06-29 10:32:56 +0200

    test

--- speech_ws/src/speech_pkg/src/speech_classification_node.py
+++ speech_ws/src/speech_pkg/src/speech_classification_node.py
@@ -52,8 +52,8 @@ class Classifier:
 
     def predict_cmd(self, signal: np.ndarray):
         x=np.reshape(signal,(1,signal.shape[0],1))
-        emb,y=self.model.predict(x)
-        #print(y[0])
+        prova,y=self.model.predict(x)
+        print(prova,y)
         l=[]
         for ele in y[0]:
             l.append("{:.13f}".format(float(ele)))
