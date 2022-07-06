2380b1d70089ca245b7696b79659f92518def2d6
pgallo17 <p.gallo17@studenti.unisa.it>
2022-07-06 10:53:28 +0200

    training_FALSE

--- speech_ws/src/speech_pkg/src/model.py
+++ speech_ws/src/speech_pkg/src/model.py
@@ -66,6 +66,6 @@ model.load_weights('../../../nosynt_cos_mean_75/distiller_ita_no_synt.h5')
 graph = tf.compat.v1.get_default_graph
 #model._make_predict_function()
 with graph.as_default():
-    _,y=model.predict(x)
+    _,y=model(x,training=False)
 
 print(y)
