6a0fcf6816fddf7ab0277fe9d3bb4fe8373c3673
pgallo17 <p.gallo17@studenti.unisa.it>
2022-07-19 13:29:43 +0200

    onnx_models

--- speech_ws/src/speech_pkg/src/speech_classification_node.py
+++ speech_ws/src/speech_pkg/src/speech_classification_node.py
@@ -59,7 +59,7 @@ class Classifier:
         '''base_path = Path(global_utils.get_curr_dir(__file__)).parent.joinpath("nosynt_cos_mean_75")
         exp_dir = base_path.joinpath("distiller_ita_no_synt.h5")
         print(exp_dir)'''
-        onnx_model = '/../../home/speech_ws/nosynt_cos_mean_75/model.onnx'
+        onnx_model = '/../../home/speech_ws/model_onnx/ita_model.onnx'
         self.session=onnxruntime.InferenceSession(onnx_model,None,providers=['CPUExecutionProvider'])
         self.input_name=self.session.get_inputs()[0].name
         self.output_name=self.session.get_outputs()[1].name
