98ec4d025f428eeaa32380678d4e993f3a42dda1
pgallo17 <p.gallo17@studenti.unisa.it>
2022-07-20 08:52:21 +0200

    arm

--- speech_ws/src/speech_pkg/src/talker_node.py
+++ speech_ws/src/speech_pkg/src/talker_node.py
@@ -1,124 +1,64 @@
 #!/usr/bin/python
 # coding=utf-8
-import rospy
 import qi
-from commands import command_eng, command_ita
-from speech_pkg.srv import *
 import argparse
-from lang_settings import AVAILABLE_LANGS
 import sys
 import time
+import rospy
 
-ENG = {
-    0: "Command predicted:",
-    1: "Probability of rejection:",
-    2: "Top 3 classes predicted:"
-}
-
-ITA = {
-    0: "Comando predetto:",
-    1: "Probabilità di rigetto:",
-    2: "Top 3 classi predette:"
-}
-
-class TextController:
-    def __init__(self, lang):
-        self.lang = lang
-        self.db_lang = ENG if self.lang == "eng" else ITA
-
-    def get_lang_string(self, index):
-        return self.db_lang[index]
-
-def get_command_str(index):
-    return commands_list[index]
-
-def get_bests(probs):
-    assert len(command_eng) == len(command_ita)
-    cmds = list(range(len(probs)))
-    values_dict = dict(zip(cmds, probs))
-    reject_key = len(command_eng)-1
-    reject_prob = round(values_dict[reject_key], 3)
-    # del values_dict[reject_key]
-    values_list = list(values_dict.items())
-    values_list.sort(key=lambda x: x[1], reverse=True)
-    bests = values_list[:N_BEST_VALUES]
-    bests = list(map(lambda x: (x[0], round(x[1], 3)), bests))
-    return bests, reject_prob
-
-def create_string(cmd, bests, reject_prob):
-    out_str = ""
-    out_str += text_controller.get_lang_string(0) + " " + get_command_str(cmd) + '\n'
-    out_str += text_controller.get_lang_string(1) + " " + str(reject_prob) + '\n'
-    out_str += text_controller.get_lang_string(2) + "\n"
-    for cmd, prob in bests:
-        out_str += "%s %s\n" % (str(prob), get_command_str(cmd))
-    return out_str
-
-def callback(req):
-    bests, reject_prob = get_bests(req.probs)
-    out_str = create_string(req.cmd, bests, reject_prob)
-    '''with open("/home/files/res.txt", "a") as fil:
-        fil.write("*"*30)
-        fil.write(out_str)
-        fil.write("*" * 30)
-        fil.write("\n")'''
-    print(out_str)
-    _,prob=bests[0]
-    if prob > 0.8 :
-        say(get_command_str(req.cmd))
-    else:
-        say(get_command_str(len(command_eng)-1))
-    
-    return TalkerResponse(True)
-
-def init_dict():
-    command_eng[len(command_eng)] = "I do not understand"
-    command_ita[len(command_ita)] = "Comando non riconosciuto"
-
-def connect_robot():
+def connect_robot(IP):
     # Connect to the robot
     print("Connecting to robot...")
     session = qi.Session()
-    session.connect('tcp://%s:9559' % IP )  # Robot IP
-    print("Robot connected")
+    try:
+        session.connect('tcp://%s:9559' % IP )  # Robot IP
+        print("Robot connected")
+    except RuntimeError:
+        print ("Can't connect to Naoqi at ip \"" + IP +".\n"
+               "Please check your script arguments. Run with -h option for help.")
+        sys.exit(1)
 
-    motion_service = session.service("ALMotion")
-    motion_service.wakeUp()
+    
 
     #TextToSpeech service
     tts = session.service("ALTextToSpeech")
     tts.setLanguage("Italian" if args.lang == "ita" else "English")
     tts.say("Ciao")
-    return tts
+    return session,tts
 
-def say(out_str):
-    try:
-        tts.say(out_str)
-    except Exception:
-        session = qi.Session()
-        session.connect('tcp://%s:9559' % IP )
 
-        tts = session.service("ALTextToSpeech")
-        tts.setLanguage("Italian" if args.lang == "ita" else "English")
-        tts.say(out_str)
-    # time.sleep(0.5)
+
+def main(session):
+    """
+    This example uses the setAngles method.
+    """
+    # Get the service ALMotion.
+
+    motion_service  = session.service("ALMotion")
+
+    motion_service.setStiffnesses("LArm", 1.0)
+
+    # Example showing how to set angles, using a fraction of max speed
+    names  = ["LShoulderPitch"]
+    angles  = [0.5]
+    fractionMaxSpeed  = 0.2
+    motion_service.setAngles(names, angles, fractionMaxSpeed)
+    
+
+    time.sleep(3.0)
+    #motion_service.setStiffnesses("LArm", 0.0)
+
 
 if __name__ == "__main__":
-    N_BEST_VALUES = 3
     parser = argparse.ArgumentParser()
-    parser.add_argument("--lang", required=True, dest="lang", type=str)
+    
     parser.add_argument("--ip", required=True, dest="ip", type=str)
+    parser.add_argument("--lang", required=True, dest="lang", type=str)
     args, unknown = parser.parse_known_args(args=rospy.myargv(argv=sys.argv)[1:])
-    IP = args.ip
-    if args.lang not in AVAILABLE_LANGS:
-        raise Exception("Selected lang not available.\nAvailable langs:", AVAILABLE_LANGS)
-    init_dict()
-    tts = connect_robot()
-    text_controller = TextController(args.lang)
-    rospy.init_node('talker')
-    commands_list = command_eng if args.lang == "eng" else command_ita
+    IP = args.ip 
+    session,tts = connect_robot(IP)
+    print('check1')
 
-
-    rospy.Service('speech_service', Talker, callback)
-
+    main(session)
+    rospy.init_node('motion')
     rospy.spin()
