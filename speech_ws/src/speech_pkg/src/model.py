ce8a4b5a277fc27aeecfdc4269233c93ecbdcb76
pgallo17 <p.gallo17@studenti.unisa.it>
2022-07-22 08:12:51 +0200

    22/07

--- speech_ws/src/speech_pkg/src/model.py
+++ speech_ws/src/speech_pkg/src/model.py
@@ -8,6 +8,7 @@ from MobileNetV3 import MobileNetV3_large
 from tensorflow.keras.layers import Conv2D, BatchNormalization, ReLU, DepthwiseConv2D, Activation, Input, Add, Lambda
 import onnx
 import onnxruntime
+import datetime
 
 
 PARAMS = {
@@ -19,12 +20,35 @@ PARAMS = {
     'mel_max_hz': 7500.0,
 }
 
+#create dataset.txt
+path='/../../home/speech_ws/Dataset_real_conv/'
+txt=open('dataset.txt','a')
+for elem in os.listdir(path):
+    isDirectory = os.path.isdir(path+elem)
+    if isDirectory:
+        for e in os.listdir(path+elem):
+            isDirectory = os.path.isdir(path+elem+'/'+e)
+            if isDirectory:
+                for sound in os.listdir(path+elem+'/'+e):
+                    wav=sound.split('.')
+                    if wav[1] == 'wav':
+                        txt.write(path+elem+'/'+e+'/'+sound+'\n')
+                    
+
+
 
 
 onnx_model_ita = '/../../home/speech_ws/model_onnx/ita_model.onnx'
 onnx_model_eng = '/../../home/speech_ws/model_onnx/eng_model.onnx'
 session_ita=onnxruntime.InferenceSession(onnx_model_ita,None,providers=['CPUExecutionProvider'])
 session_eng=onnxruntime.InferenceSession(onnx_model_eng,None,providers=['CPUExecutionProvider'])
+input_name=session_ita.get_inputs()[0].name
+output_name=session_ita.get_outputs()[1].name
+
+def tf_log10(x):
+            numerator = tf.math.log(x)
+            denominator = tf.math.log(tf.constant(10, dtype=numerator.dtype))
+            return numerator / denominator
 
 
 
@@ -45,6 +69,8 @@ f_min=PARAMS['mel_min_hz']
 f_max=PARAMS['mel_max_hz']
 log_offset=0.001
 pad_end = True
+axis=[1, 2]
+eps=1e-07
 
 assert num_fft // 2 + 1 == num_spec_bins
 lin_to_mel_matrix = tf.signal.linear_to_mel_weight_matrix(
@@ -54,49 +80,64 @@ lin_to_mel_matrix = tf.signal.linear_to_mel_weight_matrix(
     lower_edge_hertz=f_min,
     upper_edge_hertz=f_max,)
         
+seconds=[]
 
 
-def tf_log10(x):
-            numerator = tf.math.log(x)
-            denominator = tf.math.log(tf.constant(10, dtype=numerator.dtype))
-            return numerator / denominator
+path='/../../home/speech_ws/Dataset_real_conv/'
+txt=open('dataset.txt','r')
+
+for elem in txt:
+    wav=elem[:-1]
+    speech,_=librosa.load(wav,sr=sr)
+    x=np.reshape(speech,(1,speech.shape[0],1))
+    # tf.signal.stft seems to be applied along the last axis
+    second_before=datetime.datetime.now()
+    stfts = tf.signal.stft(
+        x[:,:,0], frame_length=window_length, frame_step=hop_length, pad_end=pad_end
+    )
+    mag_stfts = tf.abs(stfts)
+
+    melgrams = tf.tensordot(tf.square(mag_stfts), lin_to_mel_matrix, axes=[2, 0])
+    log_melgrams = tf_log10(melgrams + log_offset)
+    
+    mean_values = tf.math.reduce_mean(
+                log_melgrams, axis=axis, keepdims=True)
+
+    dev_std = tf.math.reduce_std(
+        log_melgrams, axis=axis, keepdims=True) + tf.constant(eps)
+    norm_tensor = (log_melgrams - mean_values)/dev_std
+
+    norm_tensor = tf.reshape(norm_tensor,(-1, norm_tensor.shape[2], 1))
+
+    norm_tensor = tf.reshape(norm_tensor,(1, norm_tensor.shape[0], norm_tensor.shape[1],norm_tensor.shape[2]))
+    
+    # This works
+    session=tf.Session()
+    with session as sess:
+        b=sess.run(norm_tensor) # ok because `sess.graph == graph`
+
+    result_ita=session_ita.run([output_name],{input_name:b})
+    second_after=datetime.datetime.now()
+    delta=second_after-second_before
+    seconds.append(delta.total_seconds())
 
 
-speech,_=librosa.load("ita_0.wav",sr=sr)
-x=np.reshape(speech,(1,speech.shape[0],1))
-# tf.signal.stft seems to be applied along the last axis
-stfts = tf.signal.stft(
-    x[:,:,0], frame_length=window_length, frame_step=hop_length, pad_end=pad_end
-)
-mag_stfts = tf.abs(stfts)
 
-melgrams = tf.tensordot(tf.square(mag_stfts), lin_to_mel_matrix, axes=[2, 0])
-log_melgrams = tf_log10(melgrams + log_offset)
+def Average(lst):
+    return sum(lst) / len(lst)
 
+average = Average(seconds)
 
+print("Average of the list =", average)
 
-axis=[1, 2]
-eps=1e-07
-mean_values = tf.math.reduce_mean(
-            log_melgrams, axis=axis, keepdims=True)
-
-dev_std = tf.math.reduce_std(
-    log_melgrams, axis=axis, keepdims=True) + tf.constant(eps)
-norm_tensor = (log_melgrams - mean_values)/dev_std
-
-norm_tensor = tf.reshape(norm_tensor,(-1, norm_tensor.shape[2], 1))
-
-norm_tensor = tf.reshape(norm_tensor,(1, norm_tensor.shape[0], norm_tensor.shape[1],norm_tensor.shape[2]))
-
-session=tf.Session()
-# This works
-with session as sess:
-  b=sess.run(norm_tensor) # ok because `sess.graph == graph`
+    
+    
 
 
 
-input_name=session.get_inputs()[0].name
-output_name=session.get_outputs()[1].name
-result=session.run([output_name],{input_name:b})
 
-print(result)
+
+
+
+
+
